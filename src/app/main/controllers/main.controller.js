83fc9b1b610d2e8820a2cc8ddfa1defb7af099df
Maadar <marcin171194@wp.pl>
2017-04-19 19:22:30 +0200

    Possibility to drag horses into blocks on the board

--- src/app/main/controllers/main.controller.js
+++ src/app/main/controllers/main.controller.js
@@ -7,22 +7,62 @@
 
   /** @ngInject */
   function MainController($scope, GetJson, Block) {
-    var blocksList = [];
-    $scope.items = new Array(49);
+    $scope.blocks = new Array(49);
+    $scope.list1 = [];
+    angular.forEach($scope.images, function(val, key) {
+      $scope.list1.push({});
+    });
+    $scope.list2 = [
+      { 'title': 'Horse1', 'url': 'assets/blackHorse.png', 'drag': true },
+      { 'title': 'Horse2', 'url': 'assets/whiteHorse.png', 'drag': true }
+    ];
 
-    for (let i=1; i<=$scope.items.length; i++) {
-      $scope.Block = new Block();
-      $scope.Block.setId(i-1);
+    $scope.startCallback = function(event, ui, title) {
+      console.log(title);
+      console.log(ui);
+      console.log(event);
+      console.log('You started draggin: ' + title.title);
+      $scope.draggedTitle = title.title;
+      console.log($scope.draggedTitle);
+    };
 
-      blocksList.push($scope.Block);
+    $scope.stopCallback = function(event, ui) {
+      console.log('Why did you stop draggin me?');
     };
-    $scope.check = function(index) {
-      angular.forEach(blocksList, function(block) {
-        if (block.id === index) {
-          console.log(block);
-        }
-      });
+
+    $scope.dragCallback = function(event, ui) {
+      console.log('hey, look I`m flying');
     };
-    console.log(blocksList);
+
+    $scope.dropCallback = function(event, ui) {
+      console.log(ui.offset);
+      console.log('hey, you dumped me :-(' , $scope.draggedTitle);
+    };
+
+    $scope.overCallback = function(event, ui) {
+      console.log('Look, I`m over you');
+    };
+
+    $scope.outCallback = function(event, ui) {
+      console.log('I`m not, hehe');
+    };
   }
+  //   var blocksList = [];
+  //   $scope.items = new Array(49);
+  //
+  //   for (let i=1; i<=$scope.items.length; i++) {
+  //     $scope.Block = new Block();
+  //     $scope.Block.setId(i-1);
+  //
+  //     blocksList.push($scope.Block);
+  //   };
+  //   $scope.check = function(index) {
+  //     angular.forEach(blocksList, function(block) {
+  //       if (block.id === index) {
+  //         console.log(block);
+  //       }
+  //     });
+  //   };
+  //   console.log(blocksList);
+  // }
 })();
--- src/app/main/directive/js/black.horse.js
+++ src/app/main/directive/js/black.horse.js
@@ -10,6 +10,9 @@
         templateUrl: 'app/main/directive/html/black.horse.html',
         link: function(scope, element, attr) {
           console.log(element);
+          scope.startCallback = function(item) {
+            console.log(item);
+          }
         }
       }
     }
