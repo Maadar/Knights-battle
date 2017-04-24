295703b18acfcf3242b56314ddc339ef48302f1d
Maadar <marcin171194@wp.pl>
2017-04-24 16:39:08 +0200

    Code refactor

--- src/app/main/controllers/main.controller.js
+++ src/app/main/controllers/main.controller.js
@@ -17,17 +17,7 @@
 
     for (let i=1; i<=$scope.blocks.length; i++) {
       $scope.Block = new Block();
-      $scope.Block.setId(i-1);
       $scope.blocksList.push($scope.Block);
-    };
-
-
-    $scope.check = function(index) {
-      angular.forEach($scope.blocksList, function(block) {
-        if (block.id === index) {
-          console.log(block);
-        }
-      });
     };
 
     //set horse position on start
@@ -38,8 +28,8 @@
 
     $scope.canJump = function(index) {
       $scope.Block.setData(index, $scope.blocksList);
-      console.log(this.Block);
       $scope.Block.displayMinusMoves();
+      console.log(this.Block);
     }
 
     $scope.cantJump = function(index) {
@@ -56,37 +46,28 @@
       console.log('Why did you stop draggin me?');
     };
 
-    $scope.dragCallback = function(event, ui, index) {
-    };
-
     var savedElements = [];
     var newArr = [];
     $scope.dropCallback = function(event, ui, index) {
       console.log("drop");
-      angular.forEach($scope.blocksList, function(element) {
-        if (index === element.id && savedElements.indexOf(element) === -1) {
-          savedElements.push(element);
-          if(savedElements.length > 2) {
-            savedElements.shift();
-          }
-        }
-      });
-
-      for (let i=0; i<$scope.blocksList.length; i++) {
-        if ($scope.blocksList[i] === savedElements[0] && savedElements.length > 1) {
-          $scope.blocksList[i].isSelected = false;
-        }
-      }
-
+      // angular.forEach($scope.blocksList, function(element) {
+      //   if (index === element.id && savedElements.indexOf(element) === -1) {
+      //     savedElements.push(element);
+      //     if(savedElements.length > 2) {
+      //       savedElements.shift();
+      //     }
+      //   }
+      // });
+      //
+      // for (let i=0; i<$scope.blocksList.length; i++) {
+      //   if ($scope.blocksList[i] === savedElements[0] && savedElements.length > 1) {
+      //     $scope.blocksList[i].isSelected = false;
+      //   }
+      // }
     };
 
     $scope.overCallback = function(event, ui, index) {
       console.log('I`m over', index);
-      angular.forEach($scope.blocksList, function(element) {
-        if (index === element.id) {
-          console.log(element);
-        }
-      });
     };
 
     $scope.outCallback = function(event, ui) {
--- src/app/main/models/block.model.js
+++ src/app/main/models/block.model.js
@@ -8,23 +8,31 @@
   /** @ngInject */
   function BlockModel() {
 
-    var Block = function(id, isSelected, canJump, possibleJumps) {
-      this.id = id || 0;
+    var Block = function(isSelected, canJump) {
       this.isSelected = true;
       this.canJump = false;
-      this.index;
-      this.blocksList;
-      this.possibleJumps = possibleJumps;
     }
 
     Block.prototype = {
-      setId: function(id) {
-        this.id = id;
-      },
-
       setData: function(index, blocksList) {
         this.index = index;
         this.blocksList = blocksList;
+      },
+
+      modify: function(possibleJumps) {
+        angular.forEach(possibleJumps, function(element) {
+          element.canJump = true;
+        });
+      },
+
+      addPossibleJumps: function(data) {
+        for (let key in data) {
+          if (data[key].indexOf(this.index) !== -1) {
+            var intKey = parseInt(key);
+            this.possibleJumps.push(this.blocksList[this.index+(intKey)]);
+          }
+        }
+        return this.possibleJumps;
       },
 
       displayPlusMoves: function() {
@@ -35,40 +43,20 @@
           "13": [1,2,3,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,34],
           "15": [0,1,2,3,4,5,7,8,9,10,11,12,14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,32,33]
         };
-
-        for (let key in plusPositions) {
-          if (plusPositions[key].indexOf(this.blocksList[this.index].id) !== -1) {
-            var intKey = parseInt(key);
-            this.possibleJumps.push(this.blocksList[this.index+intKey]);
-          }
-          angular.forEach(this.possibleJumps, function(element) {
-            element.canJump = true;
-          });
-        }
+        this.modify(this.addPossibleJumps(plusPositions));
       },
 
       displayMinusMoves: function() {
-        this.displayPlusMoves();
         var minusPositions = {
-          "15": [15,16,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,34,36,37,38,39,40,41,43,44,45,46,47,48],
-          "13": [14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,32,33,35,36,37,38,39,40,42,43,44,45,46,47],
-          "9":  [9,10,11,12,13,16,17,18,19,20,23,24,25,26,27,30,31,32,33,34,37,38,39,40,41,44,45,46,47,48],
-          "5":  [7,8,9,10,11,14,15,16,17,18,21,22,23,24,25,28,29,30,31,32,35,36,37,38,39,42,43,44,45,46]
+          "-15": [15,16,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,34,36,37,38,39,40,41,43,44,45,46,47,48],
+          "-13": [14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,32,33,35,36,37,38,39,40,42,43,44,45,46,47],
+          "-9":  [9,10,11,12,13,16,17,18,19,20,23,24,25,26,27,30,31,32,33,34,37,38,39,40,41,44,45,46,47,48],
+          "-5":  [7,8,9,10,11,14,15,16,17,18,21,22,23,24,25,28,29,30,31,32,35,36,37,38,39,42,43,44,45,46]
         };
-
-        for (let key in minusPositions) {
-          if (minusPositions[key].indexOf(this.blocksList[this.index].id) !== -1) {
-            var intKey = parseInt(key);
-            this.possibleJumps.push(this.blocksList[this.index-intKey]);
-          }
-        }
-
-        angular.forEach(this.possibleJumps, function(element) {
-          element.canJump = true;
-        });
-        console.log(this.possibleJumps);
-
+        this.displayPlusMoves();
+        this.modify(this.addPossibleJumps(minusPositions));
       }
+
     };
     return Block;
   }
