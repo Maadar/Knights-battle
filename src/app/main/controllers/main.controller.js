7051edbce7c2f4a8a77675f46540d388fce5222b
Maadar <marcin171194@wp.pl>
2017-04-24 14:29:42 +0200

    Transferred data into Model

--- src/app/main/controllers/main.controller.js
+++ src/app/main/controllers/main.controller.js
@@ -20,6 +20,8 @@
       $scope.Block.setId(i-1);
       $scope.blocksList.push($scope.Block);
     };
+
+
     $scope.check = function(index) {
       angular.forEach($scope.blocksList, function(block) {
         if (block.id === index) {
@@ -34,51 +36,10 @@
       savedElements.push($scope.blocksList[0]);
     }
 
-    var setPossibleJumps = function(possibleJumps) {
-      angular.forEach(possibleJumps, function(element) {
-        element.canJump = true;
-      });
-    }
-
-    var displayMinusMoves = function(index) {
-      var possibleJumps = [];
-      var minusPositions = {
-        "15": [15,16,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,34,36,37,38,39,40,41,43,44,45,46,47,48],
-        "13": [14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,32,33,35,36,37,38,39,40,42,43,44,45,46,47],
-        "9":  [9,10,11,12,13,16,17,18,19,20,23,24,25,26,27,30,31,32,33,34,37,38,39,40,41,44,45,46,47,48],
-        "5":  [7,8,9,10,11,14,15,16,17,18,21,22,23,24,25,28,29,30,31,32,35,36,37,38,39,42,43,44,45,46]
-      };
-
-      for (let key in minusPositions) {
-        if (minusPositions[key].indexOf($scope.blocksList[index].id) !== -1) {
-          var intKey = parseInt(key);
-          possibleJumps.push($scope.blocksList[index-intKey]);
-        }
-      }
-      setPossibleJumps(possibleJumps);
-    }
-
-    var displayPlusMoves = function(index) {
-      var possibleJumps = [];
-      var plusPositions = {
-        "5":  [2,3,4,5,6,9,10,11,12,13,16,17,18,19,20,23,24,25,26,27,30,31,32,33,34,37,38,39,40,41],
-        "9":  [0,1,2,3,4,7,8,9,10,11,14,15,16,17,18,21,22,23,24,25,28,29,30,31,32,35,36,37,38,39],
-        "13": [1,2,3,4,5,6,8,9,10,11,12,13,15,16,17,18,19,20,22,23,24,25,26,27,29,30,31,32,33,34],
-        "15": [0,1,2,3,4,5,7,8,9,10,11,12,14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,32,33]
-      };
-
-      for (let key in plusPositions) {
-        if (plusPositions[key].indexOf($scope.blocksList[index].id) !== -1) {
-          var intKey = parseInt(key);
-          possibleJumps.push($scope.blocksList[index+intKey]);
-        }
-        setPossibleJumps(possibleJumps);
-      }
-    }
-
     $scope.canJump = function(index) {
-      displayPlusMoves(index);
-      displayMinusMoves(index);
+      $scope.Block.setData(index, $scope.blocksList);
+      console.log(this.Block);
+      $scope.Block.displayMinusMoves();
     }
 
     $scope.cantJump = function(index) {
